73cfc8c5cc131dd1cf8a665316b20e6cd1d8ebbb
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-02-13 14:06:52 -0500

    test on gcc4.8

--- main.cpp
+++ main.cpp
@@ -9,18 +9,21 @@
 /*****************************************************************************/
 #include "main.h"
 #include <iostream>
-
+#include <regex>
 void A::set(const std::string &key, const std::string &v) { m_data[key] = v; }
-template <typename T> T convert(const std::string &v) {
+template<typename T>
+T convert(const std::string &v) {
   static_assert(std::is_same<T, double>::value ||
-                    std::is_same<T, std::string>::value,
+                        std::is_same<T, std::string>::value,
                 "Only std::string and double are allowed");
   return v;
 }
 
-template <> double convert(const std::string &v) { return std::stod(v); }
+template<>
+double convert(const std::string &v) { return std::stod(v); }
 
-template <typename T> T A::get(const std::string &key) const {
+template<typename T>
+T A::get(const std::string &key) const {
   if (m_data.find(key) != m_data.end()) {
     return convert<T>(m_data.at(key));
   } else {
@@ -28,12 +31,16 @@ template <typename T> T A::get(const std::string &key) const {
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   A a;
   a.set("a", "93");
   a.set("b", "abc");
   std::cout << a.get("a") << " " << a.get("b") << " " << a.get<double>("a")
             << std::endl;
-//  std::cout << a.get<int>("a") << std::endl;
+
+  std::regex trailing("(^\\s+|\\s+$)");
+  std::string target{" A "};
+  std::cout << "<" << std::regex_replace(target, trailing, {}) << ">" << std::endl;
+  //  std::cout << a.get<int>("a") << std::endl;
   return 0;
 }
